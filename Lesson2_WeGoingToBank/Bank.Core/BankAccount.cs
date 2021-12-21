672779b886ab3af50f642079b8ef58faf2edb7c1
Tarxos <ftarxos@gmail.com>
2021-12-22 02:24:28 +0300

    add custom and maybe pointless constructors to bank account and use its in console project

--- Lesson2_WeGoingToBank/Bank.Core/BankAccount.cs
+++ Lesson2_WeGoingToBank/Bank.Core/BankAccount.cs
@@ -8,11 +8,25 @@ namespace Bank.Core
         private readonly BankAccountType _type;
         private decimal _balance;
 
-        public BankAccount(BankAccountType type)
+        public BankAccount(decimal initialBalance) : this(initialBalance, BankAccountType.Deposit)
         {
+
+        }
+
+        public BankAccount(BankAccountType type) : this(0, type)
+        {
+
+        }
+
+        public BankAccount(decimal initialBalance, BankAccountType type)
+        {
+            if (initialBalance < 0) throw new ArgumentException("Initial balance cannot be lower that zero");
+            _balance = initialBalance;
             _id = Guid.NewGuid();
             _type = type;
         }
+
+        
 
         public Guid Id => _id;
         public BankAccountType Type => _type;
