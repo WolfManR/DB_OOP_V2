ba73a547a26ae72bf5e335cdfac0dd40d465ba02
Tarxos <ftarxos@gmail.com>
2021-12-22 02:32:14 +0300

    because homework end just clear code style

--- Lesson2_WeGoingToBank/Bank.Core/BankAccount.cs
+++ Lesson2_WeGoingToBank/Bank.Core/BankAccount.cs
@@ -6,17 +6,12 @@ namespace Bank.Core
     {
         private readonly Guid _id;
         private readonly BankAccountType _type;
+
         private decimal _balance;
 
-        public BankAccount(decimal initialBalance) : this(initialBalance, BankAccountType.Deposit)
-        {
+        public BankAccount(decimal initialBalance) : this(initialBalance, BankAccountType.Deposit) { }
 
-        }
-
-        public BankAccount(BankAccountType type) : this(0, type)
-        {
-
-        }
+        public BankAccount(BankAccountType type) : this(0, type) { }
 
         public BankAccount(decimal initialBalance, BankAccountType type)
         {
@@ -25,9 +20,7 @@ namespace Bank.Core
             _id = Guid.NewGuid();
             _type = type;
         }
-
         
-
         public Guid Id => _id;
         public BankAccountType Type => _type;
         public decimal Balance => _balance;
