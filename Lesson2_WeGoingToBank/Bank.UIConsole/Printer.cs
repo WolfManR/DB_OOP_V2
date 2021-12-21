c5a33377a2e70ce4fc447de9551affbbc8b7a5b0
Tarxos <ftarxos@gmail.com>
2021-12-22 02:10:01 +0300

    add console app to print bank account data

---
+++ Lesson2_WeGoingToBank/Bank.UIConsole/Printer.cs
@@ -0,0 +1,14 @@
+﻿using Bank.Core;
+using static System.Console;
+
+namespace Bank.UIConsole
+{
+    public class Printer
+    {
+        public void Print(BankAccount bankAccount)
+        {
+            WriteLine($"Bank account {bankAccount.Id} of type {bankAccount.Type}");
+            WriteLine($"Balance: {bankAccount.Balance}");
+        }
+    }
+}
---
+++ Lesson2_WeGoingToBank/Bank.UIConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using Bank.Core;
+using Bank.UIConsole;
+
+
+Printer printer = new();
+
+BankAccount bankAccount = new(BankAccountType.Deposit);
+
+bankAccount.Deposit(19.2m);
+bankAccount.Deposit(144.67m);
+bankAccount.TryWithdraw(120.2m);
+
+printer.Print(bankAccount);
