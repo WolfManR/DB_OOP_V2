0899335a54c0b9fec0c8bc431cd9f294560fc8a3
Tarxos <ftarxos@gmail.com>
2021-12-22 02:09:38 +0300

    make base bank account model and bank account types enum

---
+++ Lesson2_WeGoingToBank/Bank.Core/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bank.Core
+{
+    public class BankAccount
+    {
+        private readonly Guid _id;
+        private readonly BankAccountType _type;
+        private decimal _balance;
+
+        public BankAccount(BankAccountType type)
+        {
+            _id = Guid.NewGuid();
+            _type = type;
+        }
+
+        public Guid Id => _id;
+        public BankAccountType Type => _type;
+        public decimal Balance => _balance;
+
+        public void Deposit(decimal sum)
+        {
+            _balance += sum;
+        }
+
+        public bool TryWithdraw(decimal sum)
+        {
+            if (_balance - sum < 0) return false;
+            _balance -= sum;
+            return true;
+        }
+    }
+}
