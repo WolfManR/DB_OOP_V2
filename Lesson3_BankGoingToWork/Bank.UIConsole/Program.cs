bec130af7dadaf59b9b04ab99a90badd72ae681b
Tarxos <ftarxos@gmail.com>
2021-12-26 21:33:34 +0300

    add transfer moneys from one bank account to other behavior

--- Lesson3_BankGoingToWork/Bank.UIConsole/Program.cs
+++ Lesson3_BankGoingToWork/Bank.UIConsole/Program.cs
@@ -13,3 +13,11 @@ bankAccount.Deposit(144.67m);
 bankAccount.TryWithdraw(120.2m);
 
 printer.Print(bankAccount);
+
+const decimal transferAmount = 40m;
+string transferMessage = $"Transfer of sum {transferAmount} from {bankAccount.Id} to {bankAccount2.Id} was";
+bool transferResult = bankAccount.TryDepositTo(bankAccount2, transferAmount);
+
+Console.WriteLine($"{transferMessage} {(transferResult ? "succeed" : "failed")}");
+printer.Print(bankAccount);
+printer.Print(bankAccount2);
