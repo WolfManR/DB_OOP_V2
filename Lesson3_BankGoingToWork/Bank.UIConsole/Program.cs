527e28fbeb6dc157836fb7d38d97586c33e2a6db
Tarxos <ftarxos@gmail.com>
2021-12-26 19:27:41 +0300

    add bank code from lesson 2 and modify it to net 6

---
+++ Lesson3_BankGoingToWork/Bank.UIConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿Printer printer = new();
+
+BankAccount bankAccount = new(5m);
+BankAccount bankAccount2 = new(BankAccountType.Deposit);
+BankAccount bankAccount3 = new(10.3m, BankAccountType.Credit);
+
+printer.Print(bankAccount);
+printer.Print(bankAccount2);
+printer.Print(bankAccount3);
+
+bankAccount.Deposit(19.2m);
+bankAccount.Deposit(144.67m);
+bankAccount.TryWithdraw(120.2m);
+
+printer.Print(bankAccount);
