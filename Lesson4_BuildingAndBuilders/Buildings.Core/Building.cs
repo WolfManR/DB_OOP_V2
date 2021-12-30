12c4fbeeb3212ab0c01921f587c9c645e29bd797
Tarxos <ftarxos@gmail.com>
2021-12-31 00:15:03 +0300

    add base functionality of model Building

--- Lesson4_BuildingAndBuilders/Buildings.Core/Building.cs
+++ Lesson4_BuildingAndBuilders/Buildings.Core/Building.cs
@@ -2,5 +2,25 @@
 
 public class Building
 {
+    private readonly Guid _id;
+    private double _height;
+    private int _numberOfStoreys;
+    private int _countOfEntrances;
+    private int _countOfApartments;
 
+    public Building()
+    {
+        _id = Guid.NewGuid();
+    }
+
+    public Guid Id => _id;
+
+    public double Height { get => _height; set => _height = value; }
+    public int NumberOfStoreys { get => _numberOfStoreys; set => _numberOfStoreys = value; }
+    public int CountOfEntrances { get => _countOfEntrances; set => _countOfEntrances = value; }
+    public int CountOfApartments { get => _countOfApartments; set => _countOfApartments = value; }
+
+    public double CalculateHeightOfStorey() => Height / NumberOfStoreys;
+    public double CalculateNumberOfApartmentsInEntrace() => (double)CountOfApartments / CountOfEntrances;
+    public double CalculateNumberOfApartmentsOnStorey() => (double)CountOfApartments / NumberOfStoreys;
 }
