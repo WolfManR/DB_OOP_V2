9516026c4c40be1b6576d7e6202b4e7c7b016d9c
Tarxos <ftarxos@gmail.com>
2022-01-19 20:29:43 +0300

    add junk implementations of Draw methods from previous course

--- Lesson6_BankAndShapes/Figures.Core/Figure.cs
+++ Lesson6_BankAndShapes/Figures.Core/Figure.cs
@@ -1,4 +1,6 @@
-﻿namespace Figures.Core
+﻿using System.Drawing;
+
+namespace Figures.Core
 {
     internal abstract class Figure
     {
@@ -9,10 +11,16 @@
         public abstract void MoveVertical(int offset);
 
         public abstract void ChangeColor(Color color);
+
+        public abstract void Draw();
     }
 
     internal class Point : Figure
     {
+        public char Symbol { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
         public override void MoveHorizontal(int offset)
         {
         }
@@ -24,21 +32,84 @@
         public override void ChangeColor(Color color)
         {
         }
+
+        public override void Draw()
+        {
+        }
     }
 
     internal sealed class Circle : Point
     {
+        public double Radius { get; set; }
+        public double Thickness { get; set; }
         public double Square()
         {
             return 0;
+        }
+
+        public override void Draw()
+        {
+            var radius = Radius;
+            var thickness = Thickness;
+            double rIn = radius - thickness, rOut = radius + thickness;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write(Symbol);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 
     internal sealed class Rectangle : Point
     {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
         public double Square()
         {
             return 0;
+        }
+
+        public override void Draw()
+        {
+            string s = Symbol.ToString();
+            string space = "";
+            string temp = "";
+            for (int i = 0; i < Width; i++)
+            {
+                space += " ";
+                s += Symbol;
+            }
+
+            for (int j = 0; j < X; j++)
+                temp += " ";
+
+            s += Symbol + "\n";
+            for (int i = 0; i < Height; i++)
+                s += temp + Symbol + space + Symbol + "\n";
+
+            s += temp + Symbol;
+            for (int i = 0; i < Width; i++)
+                s += "═";
+
+            s += Symbol + "\n";
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.CursorLeft = X;
+            Console.Write(s);
+            Console.ResetColor();
         }
     }
 }
